ef751251d8da9241bd6dc93e788e3e6dada9ba81
steveafrost <ffrostt@gmail.com>
2016-09-03 16:12:06 -0400

    15m mark of TodoMVC 3

--- app/models/item.rb
+++ app/models/item.rb
@@ -2,5 +2,13 @@ class Item < ActiveRecord::Base
   # http://api.rubyonrails.org/classes/ActiveRecord/Associations/ClassMethods.html#method-i-belongs_to
   belongs_to :list # Add methods to my model.
   validates :description, :presence => true
+
+  def complete?
+  	self.status == 1
+  end
+
+  def incomplete?
+  	self.status == 0
+  end
   
 end
